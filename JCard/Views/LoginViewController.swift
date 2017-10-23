f9d8f5101b3682e753a8054ffe8db348a31cb168
민수 <suhz23@naver.com>
2017-10-23 18:04:01 +0900

    회원가입 php 작성할차례

--- JCard/Views/LoginViewController.swift
+++ JCard/Views/LoginViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 
 class LoginViewController: UIViewController {
     override func viewDidLoad() {
-        view.backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+        view.backgroundColor = UIColor.white
         setLayout()
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -23,10 +23,22 @@ class LoginViewController: UIViewController {
         view.endEditing(true)
         dismiss(animated: true, completion: nil)
     }
-    
-    @IBOutlet private weak var fieldBackground: UIView!
     @IBOutlet private weak var idField: UITextField!
     @IBOutlet private weak var pwField: UITextField!
+    
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBAction private func pressLoginButton(sender: UIButton) {
+        
+    }
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBAction private func pressSignUp(sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "SignUpViewController")
+        present(vc!, animated: true, completion: nil)
+    }
+    @IBOutlet private weak var findPassword: UIButton!
+    @IBAction private func pressFindPassword(sender: UIButton) {
+        
+    }
     
     private func setLayout() {
         let backConst = {
@@ -44,20 +56,6 @@ class LoginViewController: UIViewController {
         }
         backConst()
         
-        let fieldBackgroundConst = {
-            () in
-            self.fieldBackground.translatesAutoresizingMaskIntoConstraints = false
-            self.fieldBackground.backgroundColor = UIColor.white
-            SokDesigner().cornerRadius(obj: self.fieldBackground, value: 16)
-            
-            let top = NSLayoutConstraint(item: self.fieldBackground, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 90)
-            let leading = NSLayoutConstraint(item: self.fieldBackground, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 30)
-            let trailing = NSLayoutConstraint(item: self.fieldBackground, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -30)
-            let height = NSLayoutConstraint(item: self.fieldBackground, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
-            NSLayoutConstraint.activate([top,leading,trailing,height])
-        }
-        fieldBackgroundConst()
-        
         var fieldFontSize: CGFloat {
             if UIScreen.main.bounds.width <= 320 && UIScreen.main.bounds.height <= 560 {
                 return 12
@@ -68,18 +66,18 @@ class LoginViewController: UIViewController {
         let idFieldConst = {
             () in
             self.idField.translatesAutoresizingMaskIntoConstraints = false
-            self.idField.font = UIFont.systemFont(ofSize: fieldFontSize, weight: .medium)
+            self.idField.font = UIFont.systemFont(ofSize: fieldFontSize)
             self.idField.borderStyle = .none
             self.idField.placeholder = "Username"
             self.idField.tintColor = UIColor.black
             self.idField.textAlignment = .center
             self.idField.backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
-            SokDesigner().cornerRadius(obj: self.idField, value: 12)
+            SokDesigner().cornerRadius(obj: self.idField, value: 8)
             
             let height = NSLayoutConstraint(item: self.idField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
-            let leading = NSLayoutConstraint(item: self.idField, attribute: .leading, relatedBy: .equal, toItem: self.fieldBackground, attribute: .leading, multiplier: 1, constant: 20)
-            let trailing = NSLayoutConstraint(item: self.idField, attribute: .trailing, relatedBy: .equal, toItem: self.fieldBackground, attribute: .trailing, multiplier: 1, constant: -20)
-            let top = NSLayoutConstraint(item: self.idField, attribute: .top, relatedBy: .equal, toItem: self.fieldBackground, attribute: .top, multiplier: 1, constant: 20)
+            let leading = NSLayoutConstraint(item: self.idField, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.idField, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            let top = NSLayoutConstraint(item: self.idField, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 80)
             NSLayoutConstraint.activate([height,leading,trailing,top])
         }
         idFieldConst()
@@ -87,21 +85,75 @@ class LoginViewController: UIViewController {
         let pwFieldConst = {
             () in
             self.pwField.translatesAutoresizingMaskIntoConstraints = false
-            self.pwField.font = UIFont.systemFont(ofSize: fieldFontSize, weight: .medium)
+            self.pwField.font = UIFont.systemFont(ofSize: fieldFontSize)
             self.pwField.borderStyle = .none
             self.pwField.placeholder = "Password"
             self.pwField.isSecureTextEntry = true
             self.pwField.tintColor = UIColor.black
             self.pwField.textAlignment = .center
             self.pwField.backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
-            SokDesigner().cornerRadius(obj: self.pwField, value: 12)
+            SokDesigner().cornerRadius(obj: self.pwField, value: 8)
             
             let height = NSLayoutConstraint(item: self.pwField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
-            let leading = NSLayoutConstraint(item: self.pwField, attribute: .leading, relatedBy: .equal, toItem: self.fieldBackground, attribute: .leading, multiplier: 1, constant: 20)
-            let trailing = NSLayoutConstraint(item: self.pwField, attribute: .trailing, relatedBy: .equal, toItem: self.fieldBackground, attribute: .trailing, multiplier: 1, constant: -20)
-            let top = NSLayoutConstraint(item: self.pwField, attribute: .top, relatedBy: .equal, toItem: self.fieldBackground, attribute: .top, multiplier: 1, constant: 20+(GlobalInformation().tf_height_size*1.25)+20)
+            let leading = NSLayoutConstraint(item: self.pwField, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.pwField, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            let top = NSLayoutConstraint(item: self.pwField, attribute: .top, relatedBy: .equal, toItem: self.idField, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_font_size*1.25+30)
             NSLayoutConstraint.activate([height,leading,trailing,top])
         }
         pwFieldConst()
+        
+        var buttonFontSize: CGFloat {
+            if UIScreen.main.bounds.width <= 320 && UIScreen.main.bounds.height <= 560 {
+                return 13.5
+            } else {
+                return 17
+            }
+        }
+        let loginButtonConst = {
+            () in
+            self.loginButton.translatesAutoresizingMaskIntoConstraints = false
+            self.loginButton.setTitle(NSLocalizedString("SignIn", comment: ""), for: .normal)
+            self.loginButton.backgroundColor = GlobalInformation().color_lightBlue
+            self.loginButton.setTitleColor(UIColor.white, for: .normal)
+            self.loginButton.titleLabel?.font = UIFont.systemFont(ofSize: buttonFontSize, weight: .black)
+            SokDesigner().cornerRadius(obj: self.loginButton, value: 4)
+            
+            let height = NSLayoutConstraint(item: self.loginButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
+            let top = NSLayoutConstraint(item: self.loginButton, attribute: .top, relatedBy: .equal, toItem: self.pwField, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25+20)
+            let leading = NSLayoutConstraint(item: self.loginButton, attribute: .leading, relatedBy: .equal, toItem: self.pwField, attribute: .leading, multiplier: 1, constant: 0)
+            let trailing = NSLayoutConstraint(item: self.loginButton, attribute: .trailing, relatedBy: .equal, toItem: self.pwField, attribute: .trailing, multiplier: 1, constant: -(UIScreen.main.bounds.width/2.15))
+            NSLayoutConstraint.activate([height,top,leading,trailing])
+        }
+        loginButtonConst()
+        
+        let signUpButtonConst = {
+            () in
+            self.signUpButton.translatesAutoresizingMaskIntoConstraints = false
+            self.signUpButton.setTitle(NSLocalizedString("SignUp", comment: ""), for: .normal)
+            self.signUpButton.setTitleColor(UIColor.white, for: .normal)
+            self.signUpButton.backgroundColor = GlobalInformation().color_lightBlue
+            self.signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: buttonFontSize, weight: .black)
+            SokDesigner().cornerRadius(obj: self.signUpButton, value: 4)
+            
+            let height = NSLayoutConstraint(item: self.signUpButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
+            let top = NSLayoutConstraint(item: self.signUpButton, attribute: .top, relatedBy: .equal, toItem: self.pwField, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25+20)
+            let trailing = NSLayoutConstraint(item: self.signUpButton, attribute: .trailing, relatedBy: .equal, toItem: self.pwField, attribute: .trailing, multiplier: 1, constant: 0)
+            let leading = NSLayoutConstraint(item: self.signUpButton, attribute: .leading, relatedBy: .equal, toItem: self.pwField, attribute: .leading, multiplier: 1, constant: UIScreen.main.bounds.width/2.15)
+            NSLayoutConstraint.activate([height,top,trailing,leading])
+        }
+        signUpButtonConst()
+        
+        let findPasswordConst = {
+            () in
+            self.findPassword.translatesAutoresizingMaskIntoConstraints = false
+            self.findPassword.setTitle(NSLocalizedString("FindPassword", comment: ""), for: .normal)
+            
+            let height = NSLayoutConstraint(item: self.findPassword, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.1)
+            let leading = NSLayoutConstraint(item: self.findPassword, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.findPassword, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            let top = NSLayoutConstraint(item: self.findPassword, attribute: .top, relatedBy: .equal, toItem: self.signUpButton, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_height_size+20)
+            NSLayoutConstraint.activate([height,leading,trailing,top])
+        }
+        findPasswordConst()
     }
 }
---
+++ JCard/Views/SignUpViewController.swift
@@ -0,0 +1,195 @@
+//
+//  SignUpViewController.swift
+//  JCard
+//
+//  Created by 민수 on 2017. 10. 23..
+//  Copyright © 2017년 김민수. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RealmSwift
+
+class SignUpViewController: UIViewController {
+    override func viewDidLoad() {
+        setLayout()
+        usernameField.tag = 1
+        passwordField.tag = 2
+        passwordField2.tag = 3
+    }
+    
+    @IBOutlet private weak var backButton: UIButton!
+    @IBAction private func pressBackButton(sender: UIButton) {
+        view.endEditing(true)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet private weak var usernameField: UITextField!
+    @IBOutlet private weak var passwordField: UITextField!
+    @IBOutlet private weak var passwordField2: UITextField!
+    @IBAction private func fieldBegin(sender: UITextField) {
+        if usernameField.text!.lengthOfBytes(using: .utf8) < 4 {
+            self.saveButton.isEnabled = false
+            self.saveButton.alpha = 0.25
+        } else if passwordField.text!.lengthOfBytes(using: .utf8) < 4 {
+            self.saveButton.isEnabled = false
+            self.saveButton.alpha = 0.25
+        } else if passwordField2.text!.lengthOfBytes(using: .utf8) < 4 {
+            self.saveButton.isEnabled = false
+            self.saveButton.alpha = 0.25
+        } else {
+            self.saveButton.isEnabled = true
+            self.saveButton.alpha = 1
+        }
+    }
+    
+    @IBOutlet private weak var saveButton: UIButton!
+    @IBAction private func pressSaveButton(sender: UIButton) {
+        if usernameField.text!.lengthOfBytes(using: .utf8) > 30 {
+            let alert = UIAlertController(title: NSLocalizedString("Message", comment: ""), message: NSLocalizedString("MaxID", comment: ""), preferredStyle: .alert)
+            self.present(alert, animated: true, completion: {
+                sleep(1)
+                self.dismiss(animated: true, completion: {
+                    self.usernameField.becomeFirstResponder()
+                })
+            })
+        } else if passwordField.text!.lengthOfBytes(using: .utf8) > 50 {
+            let alert = UIAlertController(title: NSLocalizedString("Message", comment: ""), message: NSLocalizedString("MaxPW", comment: ""), preferredStyle: .alert)
+            self.present(alert, animated: true, completion: {
+                sleep(1)
+                self.dismiss(animated: true, completion: {
+                    self.passwordField.becomeFirstResponder()
+                })
+            })
+        } else if passwordField.text !=  passwordField2.text {
+            let alert = UIAlertController(title: NSLocalizedString("Message", comment: ""), message: NSLocalizedString("DoNotMatchPW", comment: ""), preferredStyle: .alert)
+            self.present(alert, animated: true, completion: {
+                sleep(1)
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            BackUpViewBrain().isConnectToNetwork(handler: {
+                result in
+                if result == false {
+                    let alert = UIAlertController(title: NSLocalizedString("Message", comment: ""), message: NSLocalizedString("NetworkFailed", comment: ""), preferredStyle: .alert)
+                    self.present(alert, animated: true, completion: {
+                        sleep(1)
+                        self.dismiss(animated: true, completion: nil)
+                    })
+                } else {
+                    print(1)
+                }
+            })
+        }
+    }
+    
+    private func setLayout() {
+        let backConst = {
+            () in
+            self.backButton.translatesAutoresizingMaskIntoConstraints = false
+            self.backButton.tintColor = UIColor.black
+            self.backButton.setTitle("", for: .normal)
+            self.backButton.setImage(UIImage(named: "left.png"), for: .normal)
+            
+            let width = NSLayoutConstraint(item: self.backButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().top_menu_size)
+            let height = NSLayoutConstraint(item: self.backButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().top_menu_size)
+            let top = NSLayoutConstraint(item: self.backButton, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: GlobalInformation().top_menu_top_size)
+            let leading = NSLayoutConstraint(item: self.backButton, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: GlobalInformation().top_menu_space)
+            NSLayoutConstraint.activate([width,height,top,leading])
+        }
+        backConst()
+        
+        var fieldFontSize: CGFloat {
+            if UIScreen.main.bounds.width <= 320 && UIScreen.main.bounds.height <= 560 {
+                return 12
+            } else {
+                return 15
+            }
+        }
+        let usernameFieldConst = {
+            () in
+            self.usernameField.translatesAutoresizingMaskIntoConstraints = false
+            self.usernameField.placeholder = NSLocalizedString("EnterID", comment: "")
+            self.usernameField.textAlignment = .center
+            self.usernameField.borderStyle = .none
+            self.usernameField.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+            self.usernameField.font = UIFont.systemFont(ofSize: fieldFontSize)
+            self.usernameField.tintColor = UIColor.black
+            SokDesigner().cornerRadius(obj: self.usernameField, value: 8)
+            
+            let height = NSLayoutConstraint(item: self.usernameField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
+            let leading = NSLayoutConstraint(item: self.usernameField, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.usernameField, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            let top = NSLayoutConstraint(item: self.usernameField, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 80)
+            NSLayoutConstraint.activate([leading,trailing,top,height])
+        }
+        usernameFieldConst()
+        
+        let passwordConst = {
+            () in
+            self.passwordField.translatesAutoresizingMaskIntoConstraints = false
+            self.passwordField.placeholder = NSLocalizedString("EnterPW", comment: "")
+            self.passwordField.textAlignment = .center
+            self.passwordField.borderStyle = .none
+            self.passwordField.tintColor = UIColor.black
+            self.passwordField.isSecureTextEntry = true
+            self.passwordField.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+            self.passwordField.font = UIFont.systemFont(ofSize: fieldFontSize)
+            SokDesigner().cornerRadius(obj: self.passwordField, value: 8)
+            
+            let height = NSLayoutConstraint(item: self.passwordField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
+            let leading = NSLayoutConstraint(item: self.passwordField, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.passwordField, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            let top = NSLayoutConstraint(item: self.passwordField, attribute: .top, relatedBy: .equal, toItem: self.usernameField, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25+20)
+            NSLayoutConstraint.activate([leading,trailing,top,height])
+        }
+        passwordConst()
+        
+        let passwordConst2 = {
+            () in
+            self.passwordField2.translatesAutoresizingMaskIntoConstraints = false
+            self.passwordField2.placeholder = NSLocalizedString("MoreCheckPassword", comment: "")
+            self.passwordField2.textAlignment = .center
+            self.passwordField2.borderStyle = .none
+            self.passwordField2.tintColor = UIColor.black
+            self.passwordField2.isSecureTextEntry = true
+            self.passwordField2.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+            self.passwordField2.font = UIFont.systemFont(ofSize: fieldFontSize)
+            SokDesigner().cornerRadius(obj: self.passwordField2, value: 8)
+            
+            let height = NSLayoutConstraint(item: self.passwordField2, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25)
+            let leading = NSLayoutConstraint(item: self.passwordField2, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.passwordField2, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            let top = NSLayoutConstraint(item: self.passwordField2, attribute: .top, relatedBy: .equal, toItem: self.passwordField, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25+20)
+            NSLayoutConstraint.activate([leading,trailing,top,height])
+        }
+        passwordConst2()
+        
+        let saveButtonConst = {
+            () in
+            self.saveButton.translatesAutoresizingMaskIntoConstraints = false
+            self.saveButton.setTitle(NSLocalizedString("SignUp", comment: ""), for: .normal)
+            self.saveButton.setTitleColor(UIColor.white, for: .normal)
+            self.saveButton.backgroundColor = UIColor.red
+            self.saveButton.isEnabled = false
+            self.saveButton.alpha = 0.25
+            
+            var fontSize: CGFloat {
+                if UIScreen.main.bounds.width <= 320 && UIScreen.main.bounds.height <= 560 {
+                    return 13.5
+                } else {
+                    return 17
+                }
+            }
+            self.saveButton.titleLabel?.font = UIFont.systemFont(ofSize: fontSize, weight: .black)
+            SokDesigner().cornerRadius(obj: self.saveButton, value: 4)
+            
+            let height = NSLayoutConstraint(item: self.saveButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: GlobalInformation().tf_height_size*1.5)
+            let top = NSLayoutConstraint(item: self.saveButton, attribute: .top, relatedBy: .equal, toItem: self.passwordField2, attribute: .top, multiplier: 1, constant: GlobalInformation().tf_height_size*1.25+30)
+            let leading = NSLayoutConstraint(item: self.saveButton, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+            let trailing = NSLayoutConstraint(item: self.saveButton, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+            NSLayoutConstraint.activate([top,height,leading,trailing])
+        }
+        saveButtonConst()
+    }
+}
